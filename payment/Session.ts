1500bf5b59314ea656269149f0958866fba069c4
Mina Badrous <minabadrous.vu@gmail.com>
2022-06-17 14:38:07 +0200

    Temp

---
+++ payment/Session.ts
@@ -0,0 +1,7 @@
+import type {
+  SessionResponse as AdyenSessionResponse,
+  CreateSessionDTO as AdyenCreateSessionDTO,
+} from '../../extension-commercetools/adyen/types/Session';
+
+export type CreateSessionDTO = AdyenCreateSessionDTO;
+export type SessionResponse = AdyenSessionResponse;
