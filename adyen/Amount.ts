c86ceb650f2b2f220ff927aab0e29d6530f233f0
Carlos Mourullo <carlos@frontastic.cloud>
2022-06-23 16:41:33 +0200

    chore(FCL-2185): moved adyen typed back to adyen folder

--- adyen/Amount.ts
+++
@@ -1,4 +0,0 @@
-export interface Amount {
-  currency: string; // The currency code compliant to ISO 4217.
-  value: number; // In cents
-}
--- payment/Session.ts
+++
@@ -1,7 +0,0 @@
-import type {
-  SessionResponse as AdyenSessionResponse,
-  CreateSessionDTO as AdyenCreateSessionDTO,
-} from '../../extension-commercetools/adyen/types/Session';
-
-export type CreateSessionDTO = AdyenCreateSessionDTO;
-export type SessionResponse = AdyenSessionResponse;
