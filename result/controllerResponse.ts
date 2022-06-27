b4a3168f665253f640bc44abb25617fac8229d4a
Andrius <xcintas@gmail.com>
2022-06-27 14:37:09 +0300

    moved type to commerce extensions

--- result/controllerResponse.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from '../../extension-commercetools/node_modules/@frontastic/extension-types/src/ts';
-
-export type ControllerResponse = Response & {
-  error?: string;
-  errorCode?: number;
-};
