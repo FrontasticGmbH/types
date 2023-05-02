d9eef598dde688b50877d91287a9e8d9babb9f47
King-Hin Leung <>
2023-05-02 10:13:45 +0800

    Refactor DynamicYieldMapper

---
+++ content/dynamicyield/Slot.ts
@@ -0,0 +1,11 @@
+export interface Slot {
+  sku : string,
+  productData : {
+    price : number,
+    name : string,
+    description : string,
+    image_url : string,
+    categories : any,
+    group_id : string
+  }
+}
