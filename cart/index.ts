024fd77a0d4f1d3b2df8856385a1aff891963a87
James Morrison <james@frontastic.cloud>
2023-09-14 17:42:28 +0100

    Added index files for domain types

---
+++ cart/index.ts
@@ -0,0 +1,26 @@
+import { Cart } from './Cart';
+import { Discount } from './Discount';
+import { LineItem } from './LineItem';
+import { Order } from './Order';
+import { Payment, PaymentStatuses } from './Payment';
+import { ShippingInfo } from './ShippingInfo';
+import { ShippingLocation } from './ShippingLocation';
+import { ShippingMethod } from './ShippingMethod';
+import { ShippingRate } from './ShippingRate';
+import { Tax } from './Tax';
+import { TaxPortion } from './TaxPortion';
+
+export {
+  Cart,
+  Discount,
+  LineItem,
+  Order,
+  Payment,
+  PaymentStatuses,
+  ShippingInfo,
+  ShippingLocation,
+  ShippingMethod,
+  ShippingRate,
+  Tax,
+  TaxPortion,
+};
---
+++ wishlist/index.ts
@@ -0,0 +1,5 @@
+import { Wishlist } from './Wishlist';
+import { LineItem } from './LineItem';
+import { Variant } from './Variant';
+
+export { Wishlist, LineItem, Variant };
